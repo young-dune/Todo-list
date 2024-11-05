264feb39b0330be7af0869bfc33ac0fb7a4a2d1f
SonYoungJun <dudwnsths1@naver.com>
2024-11-06 01:10:02 +0900

    11.06 Todo-list 저장부분(1/3) commit

--- todo.js
+++ todo.js
@@ -1,29 +1,39 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
+const myArr = [];
 
-function deleteTodo(newTodo){
-
+function saveTodos(){
+    localStorage.setItem("todos",JSON.stringify(myArr));
 }
 
-function paintTodo(newTodo){
+function deleteTodo(event){ //작성된 할 일을 삭제하는 기능
+    const li = event.target.parentElement;
+    li.remove();
+    //target은 클릭된 HTML element이다.
+    /*parentElement는 말 그대로 부모 element로 button의 
+    부모 태그인 <li></li>를 말한다. */
+}
+
+function paintTodo(newTodo){ //할 일이 입력될 때마다 작성되는 기능
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newTodo;
     const button = document.createElement("button");
     button.innerText = "❌";
-    button.addEventListener('submit',deleteTodo)
-    li.appendChild(span);
+    button.addEventListener("click",deleteTodo)
+    li.appendChild(span); //append는 항상 뒤에 마지막에 나와야한다.
     li.appendChild(button);
-    span.innerText = newTodo;
     todoList.appendChild(li);
 }
 
-function handleToSubmit(event){
+function handleToSubmit(event){ //click에 대한 event
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    myArr.push(newTodo);
     paintTodo(newTodo);
+    saveTodos();
 }
 
-todoForm.addEventListener('submit',handleToSubmit);
+todoForm.addEventListener("submit",handleToSubmit);
