6574c3f807bc99f5b3f411074f5f0e0e7aaa51bf
SonYoungJun <dudwnsths1@naver.com>
2025-03-06 22:34:37 +0900

    기능 수정 및 디자인 수정
    - 기능 수정 및 오류 해결
    - css 디자인 수정

--- todo.js
+++ todo.js
@@ -3,47 +3,51 @@ const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 let myArr = []; //Local storage속 이전의 myArr도 기억하고 싶기 때문에 const 말고 let으로 배열을 선언해준다. 
 
-function saveTodos(){
-    localStorage.setItem("todos",JSON.stringify(myArr));
-    //arr 값을 string으로 저장하고 싶을 때, JSON.stringify()를 사용한다.
+
+function saveTodos() { // 로컬 스토리지에 저장
+    localStorage.setItem("todos", JSON.stringify(myArr));
+    // arr 값을 string으로 저장하고 싶을 때, JSON.stringify()를 사용한다.
 }
 
-function deleteTodo(event){ //작성된 할 일을 삭제하는 기능
+function deleteTodo(event) { // 작성된 할 일을 삭제하는 기능
     const li = event.target.parentElement;
+    const todoText = li.querySelector("span").innerText;
+    myArr = myArr.filter(todo => todo!== todoText);
+    saveTodos(); // 로컬 스토리지에서 업데이트된 배열 저장
     li.remove();
-    //target은 클릭된 HTML element이다.
-    /*parentElement는 말 그대로 부모 element로 button의 
-    부모 태그인 <li></li>를 말한다. */
-}
+} /* target은 클릭된 HTML element이다. parentElement는 말 그대로 부모 element로 button의
+부모 태그인 <li></li>를 말한다. */
 
-function paintTodo(newTodo){ //item을 화면에 출력해주는 함수
+function paintTodo(newTodo) { // item을 화면에 출력해주는 기능
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newTodo;
     const button = document.createElement("button");
     button.innerText = "❌";
-    button.addEventListener("click",deleteTodo)
-    li.appendChild(span); //append는 항상 뒤에 마지막에 나와야한다.
+    button.addEventListener("click", deleteTodo)
+    li.appendChild(span); // append는 항상 뒤에 마지막에 나와야한다.
     li.appendChild(button);
     todoList.appendChild(li);
 }
 
-function handleToSubmit(event){ //click에 대한 event
+function handleToSubmit(event) { // click에 대한 event
     event.preventDefault();
     const newTodo = todoInput.value;
-    todoInput.value = "";
-    myArr.push(newTodo);
+    todoInput.value = ""; // 입력 칸 다시 비워주기
+
+    myArr.push(newTodo); // submit => 배열에 넣고 화면에 출력해주고 로컬 스토리지에 넣는다.
     paintTodo(newTodo);
     saveTodos();
 }
 
-todoForm.addEventListener("submit",handleToSubmit);
+todoForm.addEventListener("submit", handleToSubmit);
 
 const savedTodos = localStorage.getItem("todos");
 
-if(savedTodos !== null){
-    const parsedTodos = JSON.parse(savedTodos) //local storage에서 가져온 string을 arr로 바꿔준다.
-    myArr = parsedTodos; //새로 입력된 배열을 myArr 배열에 덮어씌운다.
-    parsedTodos.forEach(paintTodo);
-    //arrow function
+if (savedTodos !== null) {
+    const parsedTodos = JSON.parse(savedTodos) // 로컬 스토리지에서 가져온 string을 배열로 바꿔준다.
+    parsedTodos.forEach(todo => {
+        myArr.push(todo); // 로컬 스토리지에서 불러온 todo들을 myArr에 추가
+        paintTodo(todo); // 화면에 추가
+    });
 }
