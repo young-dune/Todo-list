b60fac11738a47feff9a21c78f5fb55e301912bf
SonYoungJun <dudwnsths1@naver.com>
2025-03-11 17:35:03 +0900

    주석 추가

--- todo.js
+++ todo.js
@@ -1,16 +1,24 @@
+/*
+        전체적인 흐름
+    1. 페이지 로드 시 로컬 스토리지에서 기존 할 일을 불러와서 화면에 표시
+    2. 사용자가 submit 버튼을 누르면 입력된 할 일을 myArr에 저장하고 화면에 추가한 후, 로컬 스토리지에도 저장
+    3. X 버튼을 누르면 해당 할 일이 삭제되며, myArr와 로컬 스토리지에서 함께 제거
+*/
+
+
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
-let myArr = []; //Local storage속 이전의 myArr도 기억하고 싶기 때문에 const 말고 let으로 배열을 선언해준다. 
+let myArr = []; // 로컬 스토리지속 이전의 myArr도 기억하고 싶기 때문에 const 말고 let으로 배열을 선언해준다. 
 
 
-function saveTodos() { // 로컬 스토리지에 저장
+function saveTodos() { // 로컬 스토리지에 할 일 저장
     localStorage.setItem("todos", JSON.stringify(myArr));
-    // arr 값을 string으로 저장하고 싶을 때, JSON.stringify()를 사용한다.
+    // arr 값을 string으로 저장하고 싶을 때, JSON.stringify()를 사용한다. => 브라우저의 로컬 스토리지는 문자열만 저장할 수 있음
 }
 
-function deleteTodo(event) { // 작성된 할 일을 삭제하는 기능
-    const li = event.target.parentElement;
+function deleteTodo(event) { // 작성된 할 일을 삭제
+    const li = event.target.parentElement; // 클릭한 버튼(X)의 부모 요소 li 태그를 찾는다.
     const todoText = li.querySelector("span").innerText;
     myArr = myArr.filter(todo => todo!== todoText);
     saveTodos(); // 로컬 스토리지에서 업데이트된 배열 저장
@@ -20,7 +28,7 @@ function deleteTodo(event) { // 작성된 할 일을 삭제하는 기능
 
 function paintTodo(newTodo) { // item을 화면에 출력해주는 기능
     const li = document.createElement("li");
-    const span = document.createElement("span");
+    const span = document.createElement("span"); // li 태그 생성 -> span 태그로 할 일의 텍스트 삽입
     span.innerText = newTodo;
     const button = document.createElement("button");
     button.innerText = "❌";
@@ -35,7 +43,7 @@ function handleToSubmit(event) { // click에 대한 event
     const newTodo = todoInput.value;
     todoInput.value = ""; // 입력 칸 다시 비워주기
 
-    myArr.push(newTodo); // submit => 배열에 넣고 화면에 출력해주고 로컬 스토리지에 넣는다.
+    myArr.push(newTodo); // submit => 배열에 넣고, 화면에 출력해주고, 로컬 스토리지에 넣는다.
     paintTodo(newTodo);
     saveTodos();
 }
